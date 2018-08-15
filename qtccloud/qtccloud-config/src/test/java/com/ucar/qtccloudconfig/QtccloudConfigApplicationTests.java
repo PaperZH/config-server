36ed1d0f1ba5758bac1e516d7ebb8fb80b18d011
汪洋 <yang.wang11@ucarinc.com>
2018-08-15 15:19:54 +0800

    cloudconfig

---
+++ qtccloud/qtccloud-config/src/test/java/com/ucar/qtccloudconfig/QtccloudConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ucar.qtccloudconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QtccloudConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
