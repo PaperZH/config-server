9686c394806e3d5e2729ef738f6cdff730da5b7d
李聪 <cong.li03@ucarinc.com>
2018-08-17 13:01:41 +0800

    数据显示

--- qtchome/qtchome-view/src/pages/content/details.vue
+++ qtchome/qtchome-view/src/pages/content/details.vue
@@ -2,87 +2,86 @@
   <div style="margin-top: 1%">
     <tabs-breadcrumb></tabs-breadcrumb>
     <div>
-    <el-row :gutter="20" style="margin-left: 0px; margin-right: 0px;">
-      <el-col :span="20" :offset="2"><div class="grid-content bg-purple" style="max-height:600px;">
-        <img style="max-width: 65%; display: block; float: left;" src="static/timg.jpg"/>
-        <div style="float: right;min-width: 30%;" >
-        <div style=" border-bottom: 1px solid #52464621;">
-          <div><h2>{{items.tilt}}</h2></div>
-          <div>
-            <span>技术课程</span>
-            <span >上架时间:{{items.date}}</span>
-            <i class="fa fa-thumbs-o-up">{{items.number}}</i>
-          </div>
-        </div>
-        <div>
-          <div><h4>课程上传老师</h4></div>
-          <div style="height: 116px;">
-            <div style="width: 40%;float: left"><img style="max-width: 80px;height: 70px" src="static/to.jpg"/></div>
-            <div style="width: 60%;float: left"><span>葱油饼</span><br/><br/><br/><span>congyoubing@ucarinc.com</span></div>
-          </div>
-          <div>
-            <div style=""><span>课时数：5</span><br/><br/><br/><span>有效期：从课程上架180天</span></div>
-            <div style="text-align: center; margin-top: 20%;"><el-button type="danger" round>收藏</el-button></div>
-          </div>
-        </div>
+      <el-row :gutter="20" style="margin-left: 0px; margin-right: 0px;">
+        <el-col :span="20" :offset="2"><div class="grid-content bg-purple" style="max-height:600px;">
+          <img style="max-width: 65%; display: block; float: left;" v-bind:src="items.courseCover"/>
+          <div style="float: right;min-width: 30%;" >
+            <div style=" border-bottom: 1px solid #52464621;">
+              <div><h2>{{items.courseName}}</h2></div>
+              <div>
+                <span>技术课程</span>
+                <span >上架时间:{{items.publishTime}}</span>
+                <i class="fa fa-thumbs-o-up">{{items.praiseNum}}</i>
+              </div>
+            </div>
+            <div>
+              <div><h4>课程上传老师</h4></div>
+              <div style="height: 116px;">
+                <div style="width: 40%;float: left"><img style="max-width: 80px;height: 70px" v-bind:src="teacher.imageUrl"/></div>
+                <div style="width: 60%;float: left"><span>{{teacher.username}}</span><br/><br/><br/><span>{{teacher.email}}</span></div>
+              </div>
+              <div>
+                <div style=""><span>课时数：5</span><br/><br/><br/><span>有效期：从课程上架180天</span></div>
+                <div style="text-align: center; margin-top: 20%;"><el-button type="danger" round>收藏</el-button></div>
+              </div>
+            </div>
 
-      </div>
+          </div>
 
-      </div>
-      </el-col>
-    </el-row>
+        </div>
+        </el-col>
+      </el-row>
     </div>
     <div>
-    <el-row :gutter="20" style="margin-left: 0px; margin-right: 0px;">
-      <el-col :span="20" :offset="2"><div class="grid-content bg-purple" >
-        <el-tabs type="border-card" style="height: 320px;">
-          <el-tab-pane label="课程介绍">
-            <div>
-                  一支如此彪悍、如此具有超强执行力的队伍是如何塑造出来的？他们强大的销售能力来自何处？他们铁一般的团队凝聚力来自何处？马云如何培养和带领这支队伍？阿里价值观和六脉神剑，如何在这支队伍中运用并强化…本书深度采访了众多阿里和铁军的重要人物，从马云，到一线的员工，从阿里内部，到离开铁军如今在互联网呼风唤雨的其他人物，并依据大量全新的一手资料，展现阿里铁军的人事物及其演变，展示他们如何用阿里的价值观和铁的纪律打造一个互联网地堆天团，成为日后马云口中的中国电商“黄埔军校”，让阿里铁军的文化和执行力进化、裂变与复制…
-          </div>
-          </el-tab-pane>
-          <el-tab-pane label="课程目录"><div>
-            <el-table
-              :data="tableData"
-              style="width: 100%;"
-              height="210">
-              <el-table-column
-                fixed
-                label="序列"
-                width="150" >
-                <template slot-scope="scope" >
-                  第 {{scope.$index+1}} 课
-                </template>
-              </el-table-column>
+      <el-row :gutter="20" style="margin-left: 0px; margin-right: 0px;">
+        <el-col :span="20" :offset="2"><div class="grid-content bg-purple" >
+          <el-tabs type="border-card" style="height: 320px;">
+            <el-tab-pane label="课程介绍">
+              <div>
+                   {{items.courseDescription}} 一支如此彪悍、如此具有超强执行力的队伍是如何塑造出来的？他们强大的销售能力来自何处？他们铁一般的团队凝聚力来自何处？马云如何培养和带领这支队伍？阿里价值观和六脉神剑，如何在这支队伍中运用并强化…本书深度采访了众多阿里和铁军的重要人物，从马云，到一线的员工，从阿里内部，到离开铁军如今在互联网呼风唤雨的其他人物，并依据大量全新的一手资料，展现阿里铁军的人事物及其演变，展示他们如何用阿里的价值观和铁的纪律打造一个互联网地堆天团，成为日后马云口中的中国电商“黄埔军校”，让阿里铁军的文化和执行力进化、裂变与复制…
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="课程目录"  ><div>
+              <el-table
+                :data="tableData"
+                style="width: 100%;"
+                height="210">
+                <el-table-column
+                  fixed
+                  label="序列"
+                  width="150" >
+                  <template slot-scope="scope" >
+                    第 {{scope.$index+1}} 课
+                  </template>
+                </el-table-column>
 
-              <el-table-column
-                prop="name"
-                label="课程名称"
-                width="220">
-              </el-table-column>
-              <el-table-column
-                fixed
-                prop="date"
-                label="上传日期"
-                width="230">
-              </el-table-column>
-              <el-table-column
-                prop="province"
-                label="描述"
-                width="230">
-              </el-table-column>
-              <el-table-column label="操作">
-                <template slot-scope="scope">
-                  <el-button
-                    size="mini" @click="handleClick(scope.row)"
-                    type="danger"
-                  >下载</el-button>
-                </template>
-              </el-table-column>
-
-            </el-table>
-          </div>
-            <div class="block" style="text-align: center">
+                <el-table-column
+                  prop="name"
+                  label="课程名称"
+                  width="220">
+                </el-table-column>
+                <el-table-column
+                  fixed
+                  prop="publishTime"
+                  label="上传日期"
+                  width="230">
+                </el-table-column>
+                <el-table-column
+                  prop="description"
+                  label="描述"
+                  width="230">
+                </el-table-column>
+                <el-table-column label="操作">
+                  <template slot-scope="scope">
+                    <el-button
+                      size="mini" @click="handleClick(scope.row)"
+                      type="danger"
+                    >下载</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </div>
+                <div class="block" style="text-align: center">
               <el-pagination
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
@@ -112,6 +111,7 @@
         return {
           items: {},
           currentPage4: 5,
+          teacher: { },
           tableData: [ {
             date: '2016-05-04 01:00:0',
             name: 'Java基础运算',
@@ -129,7 +129,8 @@
       },
       mounted: function () {
         this.$nextTick(function () {
-          this.items = this.$router.currentRoute.params
+          let id = this.$router.currentRoute.params.courseId
+          this.getCourseDetails(id)
         })
       },
       methods: {
@@ -141,6 +142,13 @@
         },
         handleClick (row) {
 
+        },
+        getCourseDetails (val) {
+          this.$store.dispatch('Get', {'url': `/api-home/course/getDetails/${val}`}).then(res => {
+            this.items = res.data.data
+            this.tableData = this.items.courseWare
+            this.teacher = this.items.teacher
+          })
         }
       }
     }
