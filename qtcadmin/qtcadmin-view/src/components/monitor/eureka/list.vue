583db09468a708c5bae131a0d00e6c245d075351
cong.zhou01 <cong.zhou01@ucarinc.com>
2018-08-23 16:57:52 +0800

    提交配置修改文件服务和监控服务

--- qtcadmin/qtcadmin-view/src/components/monitor/eureka/list.vue
+++ qtcadmin/qtcadmin-view/src/components/monitor/eureka/list.vue
@@ -1,3 +1,15 @@
 <template>
-  <iframe src='http://localhost:8001' id='show' width='100%' height="780px" frameborder=0 name='showHere' ></iframe>
+  <iframe :src='url' id='show' width='100%' height="780px" frameborder=0 name='showHere' ></iframe>
 </template>
+
+<script>
+  const EUREKA_SERVER = process.env.EUREKA_SERVER
+  export default {
+    data(){
+        return {
+          url: EUREKA_SERVER
+        }
+    }
+}
+</script>
+
