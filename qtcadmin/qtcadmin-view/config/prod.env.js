818cff6aa30c6b906a6d684cf4e3e7e66d1f62bc
cong.zhou01 <cong.zhou01@ucarinc.com>
2018-08-31 15:29:07 +0800

    提交修改springcloud配置

--- qtcadmin/qtcadmin-view/config/prod.env.js
+++ qtcadmin/qtcadmin-view/config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT: '"http://10.104.102.210:8002"',
-  EUREKA_SERVER: '"http://10.104.102.210:8001"'
+  API_ROOT: '"http://10.104.102.225:8002"',
+  EUREKA_SERVER: '"http://10.104.102.225:8001"'
 }
