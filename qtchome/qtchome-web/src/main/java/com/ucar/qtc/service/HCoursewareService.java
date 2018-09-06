aa6ceeb0ade2a50bfceeb6d4ab46a8b3a96a6d63
汪洋 <yang.wang11@ucarinc.com>
2018-09-06 15:07:29 +0800

    修改配置文件数据库连接

--- qtchome/qtchome-web/src/main/java/com/ucar/qtc/service/HCoursewareService.java
+++ qtchome/qtchome-web/src/main/java/com/ucar/qtc/service/HCoursewareService.java
@@ -1,25 +1,16 @@
 package com.ucar.qtc.service;
 
 import com.ucar.qtc.common.dto.config.FeignMultipartSupportConfig;
-import com.ucar.qtcassist.api.CoursewareApi;
 import com.ucar.qtcassist.api.model.BaseCoursewareListDTO;
 import com.ucar.qtcassist.api.model.CoursewareTypeDTO;
 import com.ucar.qtcassist.api.model.Result;
-import com.ucar.qtcassist.api.model.UploadCoursewareDTO;
-import feign.codec.Encoder;
-import feign.form.spring.SpringFormEncoder;
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.cloud.netflix.feign.support.SpringEncoder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 /**
@@ -39,21 +30,6 @@ public interface HCoursewareService  {
             produces = {MediaType.APPLICATION_JSON_UTF8_VALUE},consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     //@ResponseBody
     Result uploadCourseware(@RequestPart(value="file") MultipartFile file) throws Exception;
-
-//    @Configuration
-//    class MultipartSupportConfig {
-//
-//        @Autowired
-//        private ObjectFactory<HttpMessageConverters> messageConverters;
-//
-//        @Bean
-//        @Primary
-//        @Scope("prototype")
-//        public Encoder feignEncoder() {
-//            return new SpringFormEncoder(new SpringEncoder(messageConverters));
-//        }
-//
-//    }
 
     @RequestMapping(value = "/getAllType",method = RequestMethod.GET)
     Result<List<CoursewareTypeDTO>> getAllType();
