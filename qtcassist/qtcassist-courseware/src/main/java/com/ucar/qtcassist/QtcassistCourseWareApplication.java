883220b29e3c06eede2531cda93a31a0ad6c9193
汪洋 <yang.wang11@ucarinc.com>
2018-09-14 17:20:56 +0800

    升级springboot和springcloud版本

--- qtcassist/qtcassist-courseware/src/main/java/com/ucar/qtcassist/QtcassistCourseWareApplication.java
+++ qtcassist/qtcassist-courseware/src/main/java/com/ucar/qtcassist/QtcassistCourseWareApplication.java
@@ -1,12 +1,11 @@
 package com.ucar.qtcassist;
 
 import com.zuche.framework.common.SpringApplicationContext;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
--- qtcassist/qtcassist-schedule/src/main/java/com/ucar/qtcassist/schedule/service/AdminFeginClient.java
+++ qtcassist/qtcassist-schedule/src/main/java/com/ucar/qtcassist/schedule/service/AdminFeginClient.java
@@ -1,13 +1,10 @@
 package com.ucar.qtcassist.schedule.service;
 
-import com.ucar.qtcassist.api.model.Result;
 import feign.Headers;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Map;
 
 @Headers("Content-Type:application/json")
 @FeignClient("api-admin")
