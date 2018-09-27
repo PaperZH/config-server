30f33db43f26193e2037c5627b0047fddd4a8f21
cong.zhou01 <cong.zhou01@ucarinc.com>
2018-09-27 11:02:32 +0800

    删除无用导入包

--- qtcassist/qtcassist-course/src/main/java/com/ucar/qtcassist/course/service/AdminFeginClient.java
+++ qtcassist/qtcassist-course/src/main/java/com/ucar/qtcassist/course/service/AdminFeginClient.java
@@ -1,13 +1,11 @@
 package com.ucar.qtcassist.course.service;
 
 
-import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@Headers("Content-Type:application/json")
 @FeignClient(name = "api-admin")
 public interface AdminFeginClient {
 
