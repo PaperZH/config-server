0946c7c99983d6b3e6fd3d6b1bcacc157525493d
xsj <shijie.xu@ucarinc.com>
2018-09-28 11:14:33 +0800

    修改frontpage接口

--- qtcassist/qtcassist-api/src/main/java/com/ucar/qtcassist/api/CourseApi.java
+++ qtcassist/qtcassist-api/src/main/java/com/ucar/qtcassist/api/CourseApi.java
@@ -2,13 +2,14 @@ package com.ucar.qtcassist.api;
 
 import com.ucar.qtcassist.api.common.Page;
 import com.ucar.qtcassist.api.model.CoursewareDTO;
-import com.ucar.qtcassist.api.model.VO.AddCoursewareQueryVO;
 import com.ucar.qtcassist.api.model.VO.QueryVO;
 import com.ucar.qtcassist.api.model.Result;
 import com.ucar.qtcassist.api.model.VO.CourseDetailVO;
 import com.ucar.qtcassist.api.model.VO.CourseVO;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.Map;
 
@@ -104,7 +105,5 @@ public interface CourseApi {
      */
     @GetMapping("/frontPage/addCourseReadNum/{courseId}")
     Result addCourseReadNum(@PathVariable("courseId") Long courseId);
-
-
 
 }
