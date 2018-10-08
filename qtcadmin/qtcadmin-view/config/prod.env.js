6d188e0e41ae0db33dd00ba3ece5d5885334df31
guodong.zhang <guodong.zhang01@ucarinc.com>
2018-10-08 14:44:20 +0800

    '删除文件夹'

--- qtcadmin/qtcadmin-view/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  API_ROOT: '"http://10.104.108.177:8002"',
-  EUREKA_SERVER: '"http://10.104.108.177:8001"',
-  DRUID_SERVER: '"http://10.104.108.177:8004"'
-}
